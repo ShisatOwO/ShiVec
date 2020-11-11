c418681b98698231435412f6a8be979184d45c4a
Shisato <63296309+ShisatOwO@users.noreply.github.com>
2020-11-11 17:24:13 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyVec',
+    version='v0.1',
+    packages=[''],
+    url='https://github.com/ShisatOwO/PyVec',
+    license='MIT License',
+    author='Shisato',
+    author_email='',
+    description='PyVec is a simple python package for working with vectors.'
+)
